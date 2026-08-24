6a89c6ebcccd2e6aaa29d4c2904eabdc0bed19af
lrodeveloperr <lrodeveloperr@gmail.com>
2026-08-24 14:43:43 -0700

    Use lifecycle-compose owner for banner lifecycle

--- app/src/main/java/studio/gooduse/kitchenprep/monetization/AdBanner.kt
+++ app/src/main/java/studio/gooduse/kitchenprep/monetization/AdBanner.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.google.android.gms.ads.*
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
