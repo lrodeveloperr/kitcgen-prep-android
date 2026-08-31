44a3ba89b86a6ae963a664ea979be22dd66e9c32
lrodeveloperr <lrodeveloperr@gmail.com>
2026-08-31 15:06:55 -0400

    test: cover salted Kitchen timer notification IDs

--- flutter_app/test/services/timer_notifications_test.dart
+++ flutter_app/test/services/timer_notifications_test.dart
@@ -2,9 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kitchen_prep_board/services/timer_notifications.dart';
 
 void main() {
-  test('timer notification IDs are deterministic and collision aware', () {
+  test('timer notification IDs are salted and collision aware', () {
     final base = resolveTimerNotificationId('task-1', const {});
+    final expected = Object.hash('task-1', 'timer_notification') & 0x7fffffff;
 
+    expect(base, expected);
     expect(resolveTimerNotificationId('task-1', const {}), base);
     expect(
       resolveTimerNotificationId('task-1', {base: 'another-task'}),
