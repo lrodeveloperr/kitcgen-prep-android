84f44238544d8327e17a4104d03a48506fd91167
lrodeveloperr <lrodeveloperr@gmail.com>
2026-09-02 03:07:05 -0400

    fix: import iOS page transitions

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
